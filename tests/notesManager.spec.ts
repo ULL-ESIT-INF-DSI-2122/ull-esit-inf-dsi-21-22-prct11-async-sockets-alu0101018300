3cef3280fbbddabb5db58102c3cac4f98864dcbc
alu0101018300 <alu0101018300@ull.edu.es>
2022-05-08 21:07:37 +0000

    Pruebas finalizadas

---
+++ tests/notesManager.spec.ts
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import 'mocha';
+import {NotesManager} from '../src/notesManager';
+
+const testNote = new NotesManager();
+describe('Tests NotesManager Methods', () => {
+  it('There must be a class Notes', () => {
+    expect(NotesManager).to.exist;
+  });
+  it('Add note test', () => {
+    expect(testNote.addNote('Adrian', 'Prueba', 'Esto es una prueba', 'yellow').state).to.equal(1);
+  });
+  it('Read Note test', () => {
+    expect(testNote.readNote('Adrian', 'Prueba').state).to.equal(1);
+  });
+  it('Edit test4 green must work', () => {
+    expect(testNote.editNote('Adrian', 'Prueba', 'Esto es una modificacion', 'green').state).to.equal(1);
+  });
+  it('Read test6 yellow must work', () => {
+    expect(testNote.listNotes('Adrian').state).to.equal(1);
+  });
+  it('Read test8 blue must work', () => {
+    expect(testNote.removeNote('Adrian', 'Prueba').state).to.equal(1);
+  });
+});
